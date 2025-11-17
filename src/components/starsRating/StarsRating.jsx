e12aaa30cc959ca9d9e5cf5ead1680f4a8c76b63
rakinu21 <ralphkinethugdamin@gmail.com>
2025-11-17 11:30:45 +0800

    done rating star

--- src/components/starsRating/StarsRating.jsx
+++ src/components/starsRating/StarsRating.jsx
@@ -3,11 +3,44 @@ import { LiaStarSolid } from "react-icons/lia";
 import './style.css'
 
 const StarsRating = ({noOfStars = 10}) => {
+  
+  const [rating, setRating] = useState(0);
+  const [hover , setHover] = useState(0)
 
+  const handleOnclick = (getCurrentId)=>{
+     
+      setRating(getCurrentId)
+  }
+
+  const handlemouseMove = (getCurrentId)=>{
+    
+       setHover(getCurrentId)
+  }
+
+  const handleMouseLeave = ()=>{
+        setHover(rating)
+
+  }
 
   return (
     <div id='stars'>
-
+       {
+        [...Array(noOfStars)].map((stars, index)=>{
+           index += 1
+          return (
+            <LiaStarSolid
+             key={index}
+              onClick={()=> handleOnclick(index)}
+             onMouseMove={()=> handlemouseMove(index)}
+               onMouseLeave={()=> handleMouseLeave()}
+             size={50}
+             className={index <= (hover || rating) ? 'active' : 'inactive'}
+            />
+          )
+        })
+        
+        }
+       
     </div>
   )
 }
